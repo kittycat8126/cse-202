dfde6f406f9fa894681a524ce6ed47ef515758b4
Chirag <himselfchirag@gmail.com>
2024-11-14 12:52:30 +0530

    Exception Handeling

---
+++ Exception_Handeling/first.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Eneter the values of a and b : \n";
+    cin>>a>>b;
+    int x = a-b;
+    try
+    {
+        if(x!=0)
+        {
+            cout<<"Result : "<<a/x;
+        }
+        else{
+            throw(x);
+        }
+    }
+    catch(int i)
+    {
+        cout<<"Exception occured \n";
+        cout<<"Value of x : "<<x;
+    }
+    return 0;
+}
